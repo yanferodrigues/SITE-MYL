853b9a49cf02f45f0f74d143213acdc1b9fea9b7
Yan felipe <yanfelipe678@gmail.com>
2026-02-19 11:50:40 -0300

    alteração escritorio 15:30 19/02/2026

--- GYM/admin.py
+++ GYM/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from gym.models import Day
+from gym.models import Day, MuscleGroup,Exercise
 
 admin.site.register(Day)
+admin.site.register(MuscleGroup)
+admin.site.register(Exercise)
--- GYM/models.py
+++ GYM/models.py
@@ -1,8 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Day(models.Model):
+    user = models.ForeignKey(User, related_name="gym_days" ,on_delete=models.CASCADE)
     nome = models.CharField(max_length=20)
     
     def __str__(self):
         return self.nome
     
+class MuscleGroup(models.Model):
+    MUSCLE_GROUP_CHOICES = [
+    ("CHEST", "Chest"),
+    ("BACK", "Back"),
+    ("TRICEPS", "Triceps"),
+    ("BICEPS", "Biceps"),
+    ("SHOULDERS", "Shoulders"),
+    ("FOREARM", "Forearm"),
+    ("TRAPS", "Traps"),
+    ("QUADS", "Quads"),
+    ("HAMSTRINGS", "Hamstrings"),
+    ("GLUTES", "Glutes"),
+    ("CALVES", "Calves"),
+    ("ADDUCTORS", "Adductors"),
+    ("ABDUCTORS", "Abductors"),
+    ("ABS", "Abs"),
+    ]
+
+    nome = models.CharField(max_length=20, choices=MUSCLE_GROUP_CHOICES, default="GENERAL")
+    dia = models.ForeignKey(Day, related_name="muscle_groups", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.nome} - {self.dia}'
+    
+class Exercise(models.Model):
+    sets = models.IntegerField(default='1')
+    reps = models.IntegerField(default='1')
+    nome = models.CharField(max_length=100)
+    muscle_group = models.ForeignKey(MuscleGroup, related_name="exercises", on_delete=models.CASCADE)
