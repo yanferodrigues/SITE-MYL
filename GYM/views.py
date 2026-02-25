219007435f56c54fd2c2ca7d8f93f3ffb6891c02
Yan felipe <yanfelipe678@gmail.com>
2026-02-25 11:59:16 -0300

    alteração escritorio 11:30 25/02/2026

--- GYM/views.py
+++ GYM/views.py
@@ -1,72 +1,72 @@
-from django.shortcuts import render
-from gym.models import Workout,MuscleGroup,Exercise
+from django.shortcuts import render, redirect
+from gym.models import Workout, MuscleGroup, Exercise
 from django.contrib.auth.models import User
 
 def gym(request):
+
     user = User.objects.get(username="yanfelipe")
-    workouts = Workout.objects.all().filter(user=user).prefetch_related("muscle_groups__exercises")
 
     week_days = [
-    "MONDAY",
-    "TUESDAY",
-    "WEDNESDAY",
-    "THURSDAY",
-    "FRIDAY",
-    "SATURDAY",
-    "SUNDAY"
+        "MONDAY",
+        "TUESDAY",
+        "WEDNESDAY",
+        "THURSDAY",
+        "FRIDAY",
+        "SATURDAY",
+        "SUNDAY"
     ]
 
-    workout_by_day = {w.day: w for w in workouts}
-    
-    days_of_workout = []
-    
-    for day in week_days:
-        days_of_workout.append({
-            "name":day,
-            "workout":workout_by_day.get(day)
-        })
-
     if request.method == "POST":
+
         workout_day_post = request.POST.get("workout_day")
         muscle_group_post = request.POST.get("muscle_group")
         exercises_post = request.POST.getlist("exercise[]")
         sets_post = request.POST.getlist("sets[]")
         reps_post = request.POST.getlist("reps[]")
 
-        workout = Workout.objects.get(user = user, day = workout_day_post)
-        muscle_group = MuscleGroup.objects.filter(name = muscle_group_post , workout = workout)
-        
-        muscle_group.delete()
-        
-        new_muscle_group = MuscleGroup.objects.create(
-            name = muscle_group_post,
-            workout = workout
+        workout, created = Workout.objects.get_or_create(
+            user=user,
+            day=workout_day_post
         )
-        
-        for sets, reps, exercise in zip(sets_post,reps_post, exercises_post):
-            
-            Exercise.objects.create(
-                muscle_group = new_muscle_group,        
-                name = exercise,
-                sets = sets,
-                reps = reps
-            )
-        
-        
-        workouts = Workout.objects.all().filter(user=user).prefetch_related("muscle_groups__exercises")
 
-        workout_by_day = {w.day: w for w in workouts}
-        
-        days_of_workout = []
-        
-        for day in week_days:
-            days_of_workout.append({
-                "name":day,
-                "workout":workout_by_day.get(day)
-            })
-            
-    
-    return render(request,"gym.html", {
-        "days":days_of_workout
-        })
+        old_group = MuscleGroup.objects.filter(
+            name=muscle_group_post,
+            workout=workout
+        ).first()
+
+        if old_group:
+            old_group.delete()
+
+        new_muscle_group = MuscleGroup.objects.create(
+            name=muscle_group_post,
+            workout=workout
+        )
+
+        for sets, reps, exercise in zip(sets_post, reps_post, exercises_post):
+            Exercise.objects.create(
+                muscle_group=new_muscle_group,
+                name=exercise,
+                sets=sets,
+                reps=reps
+            )
+
+        return redirect("gym")
+
+    workouts = Workout.objects.filter(user=user).prefetch_related(
+        "muscle_groups__exercises"
+    )
+
+    workout_by_day = {w.day: w for w in workouts}
+
+    days_of_workout = [
+        {
+            "name": day,
+            "workout": workout_by_day.get(day)
+        }
+        for day in week_days
+    ]
+
+    return render(request, "gym.html", {
+        "days": days_of_workout
+    })
     
--- setup/static/assets/js/gym.js
+++ setup/static/assets/js/gym.js
@@ -27,36 +27,54 @@ function removeGymRowByButton(button) {
     }
 }
 
-function addNewForm() {
-    const container = document.getElementById("overlay-gym-general-forms");
+function addNewForm(button) {
+
+    const overlayContent = button.closest(".overlay-container-content");
+    const container = overlayContent.querySelector(".overlay-gym-general-forms");
+
     const form = container.querySelector(".overlay-container-content-gym-form");
     const newForm = form.cloneNode(true);
-    const exercisesRows = newForm.querySelector(".gymExercisesContainer")
+
+    const exercisesRows = newForm.querySelector(".gymExercisesContainer");
 
     while (exercisesRows.children.length > 1) {
-        exercisesRows.lastElementChild.remove()
+        exercisesRows.lastElementChild.remove();
     }
 
     newForm.querySelectorAll("input").forEach(input => {
-        input.value = "";
+        if (
+            input.name !== "workout_day" &&
+            input.name !== "csrfmiddlewaretoken"
+        ) {
+            input.value = "";
+        }
     });
 
-    container.appendChild(newForm)
+    newForm.querySelectorAll("select").forEach(select => {
+        select.value = "";
+
+    });
+
+    container.appendChild(newForm);
 }
 
 function removeForm(button) {
-    const container = document.getElementById("overlay-gym-general-forms");
+
     const form = button.closest(".overlay-container-content-gym-form");
+    const container = form.closest(".overlay-gym-general-forms");
+
     if (container.children.length > 1) {
         form.remove();
-    }
-    else {
+    } else {
         form.querySelectorAll("input").forEach(input => {
-            input.value = "";
-        })
+            if (
+                input.name !== "workout_day" &&
+                input.name !== "csrfmiddlewaretoken"
+            ) {
+                input.value = "";
+            }
+        });
     }
-    
-    
 }
 
 document.addEventListener("input", function (e) {
