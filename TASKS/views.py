563026f57fd496053fed707d8e07417bc55e8c89
Yan felipe <yanfelipe678@gmail.com>
2026-03-05 14:02:01 -0300

    alteração escritorio 14:00 05/03/2026

--- TASKS/views.py
+++ TASKS/views.py
@@ -5,9 +5,8 @@ from django.contrib.auth.models import User
 
 def tasks(request):
     user = User.objects.get(username = "yanfelipe")
+    all_tasks = Events.objects.filter(user = user) 
     today = date.today()
-    general_tasks = Events.objects.filter(user = user)
-
     days_map = {
             "MONDAY": 0,
             "TUESDAY": 1,
@@ -17,11 +16,30 @@ def tasks(request):
             "SATURDAY": 5,
             "SUNDAY": 6,
     }
+
+
+    passed_tasks = []
+    for event in all_tasks:
+        if event.date < today:
+            passed_tasks.append(event)
+        elif event.done == True:
+            passed_tasks.append(event)
+ 
+
+    general_tasks = [] 
+    for event in all_tasks:
+        if event not in passed_tasks:
+            general_tasks.append(event)
+        else:
+            pass
+
+
     today_tasks = []
     for event in general_tasks:
         repeat_list = []
         for option in event.weekly_options:
             repeat_list.append(days_map.get(option))
+
         if event.date == today:
             today_tasks.append(event)
         elif event.weekly and today.weekday() in repeat_list:
@@ -30,18 +48,16 @@ def tasks(request):
             today_tasks.append(event)
         elif event.yearly and (event.date.day == today.day and event.date.month == today.month):
             today_tasks.append(event)
-            
-    weekly_tasks = general_tasks.filter(weekly = True)
-    monthly_tasks = general_tasks.filter(monthly = True)
-    yearly_tasks = general_tasks.filter(yearly = True)
-    
-    passed_tasks = []
-    for event in Events.objects.all():
-        if event.date < today:
-            passed_tasks.append(event)
-        elif event.done == True:
-            passed_tasks.append(event)
 
+
+    weekly_tasks = all_tasks.filter(weekly = True)
+
+
+    monthly_tasks = all_tasks.filter(monthly = True)
+
+
+    yearly_tasks = all_tasks.filter(yearly = True)
+        
         
     return render(request, "tasks.html", {
         "general_tasks":general_tasks,
